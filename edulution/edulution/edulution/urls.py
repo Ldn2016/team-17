775c4901a42794e5ee8a850c068ee45727c000d4
HugoDelval <hugodelval@gmail.com>
2016-12-10 02:43:44 +0000

    templates running with django

--- edulution/edulution/edulution/urls.py
+++ edulution/edulution/edulution/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # /student/
     url(r'^$', views.index, name='index'),
+    # /student/...
+    url(r'^subjects/$', views.index, name='index'),
+
 ]
--- edulution/edulution/settings.py
+++ edulution/edulution/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -55,7 +54,7 @@ ROOT_URLCONF = 'edulution.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'edulution', 'edulution', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
