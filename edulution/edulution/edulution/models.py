2e4bf6d661610ee2f3fc40c854d5c805f62ae63b
HugoDelval <hugodelval@gmail.com>
2016-12-10 02:04:31 +0000

    adding student to module link

--- edulution/edulution/edulution/models.py
+++ edulution/edulution/edulution/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 
 
-class User(models.Model):
+class Student(models.Model):
     username = models.CharField()
+    modules = models.ManyToManyField(
+        Module,
+        through='StudentModule',
+        through_fields=('student', 'module'),
+    )
+
+
+class StudentModule(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    nb_exo_succeded = models.PositiveIntegerField()
+    status = models.PositiveIntegerField() # number of times the student passed the test associated with the module
 
 
 # like maths, literature
