552ded08b5e714d0c7a96946a8ad74d10ba0bd0d
HugoDelval <hugodelval@gmail.com>
2016-12-10 07:43:54 +0000

    adding test endpoint

--- edulution/edulution/edulution/urls.py
+++ edulution/edulution/edulution/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     url(r'^subjects/$', views.subjects, name='subjects'),
 	# /student/exercise
     url(r'^exercise/$', views.exercise, name='exercise'),
+	# /student/test/1
+    url(r'^test/(?P<module_id>\d+)$', views.test, name='test'),
 
 ]
